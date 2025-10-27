2bbf469aafbca4f860678d3e43647a4b4e0b9e76
BonaneNIYIGENA <b.niyigena@alustudent.com>
2025-10-27 10:19:49 +0200

    task done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,29 +6,18 @@ import requests
 def top_ten(subreddit):
     """Queries Reddit API and prints titles of first 10 hot posts."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "python:alx:task1 (by /u/yourusername)"}
     params = {"limit": 10}
 
-    try:
-        response = requests.get(
-            url, headers=headers, params=params, allow_redirects=False
-        )
-    except Exception:
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
+        if posts:
+            for post in posts[:10]:
+                print(post.get("data", {}).get("title"))
+        else:
+            print("None")
+    else:
         print("None")
-        return
-
-    if response.status_code != 200:
-        print("None")
-        return
-
-    data = response.json()
-    posts = data.get("data", {}).get("children", [])
-
-    if not posts:
-        print("None")
-        return
-
-    for post in posts:
-        title = post.get("data", {}).get("title")
-        if title:
-            print(title)
